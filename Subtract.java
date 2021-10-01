700ad0319fcd2b8ff1b83cfc6310712391a73eeb
sarthaksharma600 <sarthaksharma600@gmail.com>
2021-10-01 21:43:15 +0530

    util package needed and scanner object need to be closed

--- Subtract.java
+++ Subtract.java
@@ -1,3 +1,5 @@
+// need to import scanner class from util package
+import java.util.Scanner;
 class Sub
 {
 public static void main(String[] arg)
@@ -6,8 +8,12 @@ int a,b,c;
 Scanner s=new Scanner(System.in);
 System.out.println("Enter first number");
 a=s.nextInt();
+// need to close scanner object
+s.close();
 System.out.println("Enter second number");
 b=s.nextInt();
+// need to close scanner object
+s.close();
 if(a>b)
 	c=a-b;
 else
