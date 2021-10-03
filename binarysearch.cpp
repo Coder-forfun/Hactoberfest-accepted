9c54d267da031d373784bc893525fba152e71527
ALOK PANDEY <alokpandey1131@gmail.com>
2021-10-03 16:52:10 +0530

    added the cheatsheet for developer and programmer

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,151 +1,33 @@
+// Binary Search in C++
 
-#include <algorithm>
 #include <iostream>
-  
 using namespace std;
-  
-void show(int a[], int arraysize)
-{
-    for (int i = 0; i < arraysize; ++i)
-        cout << a[i] << ",";
-}
-  
-int main()
-{
-    int a[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
-    int asize = sizeof(a) / sizeof(a[0]);
-    cout << "\nThe array is : \n";
-    show(a, asize);
-  
-    cout << "\n\nLet's say we want to search for ";
-    cout << "\n2 in the array So, we first sort the array";
-    sort(a, a + asize);
-    cout << "\n\nThe array after sorting is : \n";
-    show(a, asize);
-    cout << "\n\nNow, we do the binary search";
-    if (binary_search(a, a + 10, 2))
-        cout << "\nElement found in the array";
-    else
-        cout << "\nElement not found in the array";
-  
-    cout << "\n\nNow, say we want to search for 10";
-    if (binary_search(a, a + 10, 10))
-        cout << "\nElement found in the array";
-    else
-        cout << "\nElement not found in the array";
-  
-    return 0;
-}
-=======
-/// main
-#include <bits/stdc++.h>
-using namespace std;
-  
-//define array globally
-const int N = 1e6 +4;
-  
-int a[N];
-int n;//array size
-  
-//elememt to be searched in array
-   int k;
-  
-bool check(int dig)
-{
-    //element at dig position in array
-    int ele=a[dig];
-  
-    //if k is less than
-    //element at dig position 
-    //then we need to bring our higher ending to dig
-    //and then continue further
-    if(k<=ele)
-    {
-        return 1;
-    }
-    else
-    {
-    return 0;
-    }
-}
-void binsrch(int lo,int hi)
-{
-    while(lo<hi)
-    {
-        int mid=(lo+hi)/2;
-        if(check(mid))
-        {
-            hi=mid;
-        }
-        else
-        {
-            lo=mid+1;
-        }
-    }
-    //if a[lo] is k
-    if(a[lo]==k)
-        cout<<"Element found at index "<<lo;// 0 based indexing
-    else
-        cout<<"Element doesnt exist in array";//element was not in our array
-  
-}
-  
-  
-int main()
-{   
-    cin>>n;
-   for(int i=0; i<n; i++)
-   {
-    cin>>a[i];
-   }
-   cin>>k;
-  
-   //it is being given array is sorted
-   //if not then we have to sort it
-  
-   //minimum possible point where our k can be is starting index
-   //so lo=0 
-   //also k cannot be outside of array so end point
-   //hi=n
-  
-   binsrch(0,n);
-=======
-#include <algorithm>
-#include <iostream>
-  
-using namespace std;
-  
-void show(int a[], int arraysize)
-{
-    for (int i = 0; i < arraysize; ++i)
-        cout << a[i] << ",";
-}
-  
-int main()
-{
-    int a[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
-    int asize = sizeof(a) / sizeof(a[0]);
-    cout << "\nThe array is : \n";
-    show(a, asize);
-  
-    cout << "\n\nLet's say we want to search for ";
-    cout << "\n2 in the array So, we first sort the array";
-    sort(a, a + asize);
-    cout << "\n\nThe array after sorting is : \n";
-    show(a, asize);
-    cout << "\n\nNow, we do the binary search";
-    if (binary_search(a, a + 10, 2))
-        cout << "\nElement found in the array";
-    else
-        cout << "\nElement not found in the array";
-  
-    cout << "\n\nNow, say we want to search for 10";
-    if (binary_search(a, a + 10, 10))
-        cout << "\nElement found in the array";
-    else
-        cout << "\nElement not found in the array";
 
-  
-    return 0;
+int binarySearch(int array[], int x, int low, int high) {
+  if (high >= low) {
+    int mid = low + (high - low) / 2;
+
+
+    if (array[mid] == x)
+      return mid;
+
+    // Search the left half
+    if (array[mid] > x)
+      return binarySearch(array, x, low, mid - 1);
+
+    return binarySearch(array, x, mid + 1, high);
+  }
+
+  return -1;
 }
 
+int main(void) {
+  int array[] = {3, 4, 5, 6, 7, 8, 9};
+  int x = 4;
+  int n = sizeof(array) / sizeof(array[0]);
+  int result = binarySearch(array, x, 0, n - 1);
+  if (result == -1)
+    printf("Not found");
+  else
+    printf("Element is found at index %d", result);
+}
--- quicksort.cpp
+++ quicksort.cpp
@@ -1,74 +1,73 @@
-#include <bits/stdc++.h>
+// Quick sort in C++
+
+#include <iostream>
 using namespace std;
 
-// A utility function to swap two elements
-void swap(int* a, int* b)
-{
-	int t = *a;
-	*a = *b;
-	*b = t;
+// Swap Two Elements
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
 }
 
-/* This function takes last element as pivot, places
-the pivot element at its correct position in sorted
-array, and places all smaller (smaller than pivot)
-to left of pivot and all greater elements to right
-of pivot */
-int partition (int arr[], int low, int high)
-{
-	int pivot = arr[high]; // pivot
-	int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
-
-	for (int j = low; j <= high - 1; j++)
-	{
-		// If current element is smaller than the pivot
-		if (arr[j] < pivot)
-		{
-			i++; // increment index of smaller element
-			swap(&arr[i], &arr[j]);
-		}
-	}
-	swap(&arr[i + 1], &arr[high]);
-	return (i + 1);
+// Print the Array
+void printArray(int array[], int size) {
+  int i;
+  for (i = 0; i < size; i++)
+    cout << array[i] << " ";
+  cout << endl;
 }
 
-/* The main function that implements QuickSort
-arr[] --> Array to be sorted,
-low --> Starting index,
-high --> Ending index */
-void quickSort(int arr[], int low, int high)
-{
-	if (low < high)
-	{
-		/* pi is partitioning index, arr[p] is now
-		at right place */
-		int pi = partition(arr, low, high);
+//rearrange array 
+int partition(int array[], int low, int high) {
+    
+  // rightmost element as pivot
+  int pivot = array[high];
+  
+  // pointer for greater element
+  int i = (low - 1);
 
-		// Separately sort elements before
-		// partition and after partition
-		quickSort(arr, low, pi - 1);
-		quickSort(arr, pi + 1, high);
-	}
+  /*traverse each element of the array compare them with the pivot */
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) { 
+      i++; // if element smaller than pivot is found
+      // swap it with the greater element pointed by i
+      swap(&array[i], &array[j]);
+    }
+  }
+  
+  // swap pivot with the greater element at i
+  swap(&array[i + 1], &array[high]);
+  
+  // return the partition point
+  return (i + 1);
 }
 
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-	int i;
-	for (i = 0; i < size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+
+    int pi = partition(array, low, high);
+
+    // recursive call on the left of pivot
+    quickSort(array, low, pi - 1);
+
+    // recursive call on the right of pivot
+    quickSort(array, pi + 1, high);
+  }
 }
 
-// Driver Code
-int main()
-{
-	int arr[] = {10, 7, 8, 9, 1, 5};
-	int n = sizeof(arr) / sizeof(arr[0]);
-	quickSort(arr, 0, n - 1);
-	cout << "Sorted array: \n";
-	printArray(arr, n);
-	return 0;
+// Driver code
+int main() {
+  int data[] = {2,4,6,1,8,20,5};
+  int n = sizeof(data) / sizeof(data[0]);
+  
+  cout << "Unsorted Array: \n";
+  printArray(data, n);
+
+  quickSort(data, 0, n - 1);
+  
+  cout << "Sorted array in ascending order: \n";
+  printArray(data, n);
 }
 
-// This code is contributed by Uttam
+//Done By Atishi Jain
