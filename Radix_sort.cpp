8637de8f8a770cf18ea68270548f278986f3ced6
kartik-raj7 <72154895+kartik-raj7@users.noreply.github.com>
2021-10-03 19:46:02 +0530

    Create Radix_sort.cpp

---
+++ Radix_sort.cpp
@@ -0,0 +1,41 @@
+// radix sort
+#include <iostream>
+using namespace std;
+int getMax(int arr[], int n)
+{
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+void countSort(int arr[], int n, int exp)
+{
+    int output[n]; 
+    int i, count[10] = { 0 };
+    for (i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+    for (i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+void radixsort(int arr[], int n)
+{
+    int m = getMax(arr, n);
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
+int main()
+{
+    int arr[] = { 17, 4, 5, 9, 22, 24, 122, 66 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+      radixsort(arr, n);
+   return 0;
+}
