2506062c3c7d7abc4a2f6a8c8d17378fbbe165d7
OarabileMwiya <42150074+OarabileMwiya@users.noreply.github.com>
2021-10-03 14:56:54 +0200

    Create insertion.java

---
+++ Java/insertion.java
@@ -0,0 +1,20 @@
+
+
+    /*Function to sort array using insertion sort*/
+    static void sort(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 1; i < n; ++i) { 
+            int key = arr[i]; 
+            int j = i - 1; 
+
+            /* Move elements of arr[0..i-1], that are 
+            greater than key, to one position ahead 
+            of their current position */
+            while (j >= 0 && arr[j] > key) { 
+                arr[j + 1] = arr[j]; 
+                j = j - 1; 
+            } 
+            arr[j + 1] = key; 
+        } 
+    }
