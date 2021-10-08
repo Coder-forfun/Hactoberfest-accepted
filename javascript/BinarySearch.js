bd2c94dc4b4448559468a9337ad27440beda6413
Jiyan Patil <76421551+jiyanpatil07@users.noreply.github.com>
2021-10-08 11:07:46 +0530

    Added BinarySearch Scripts

---
+++ javascript/BinarySearch.js
@@ -0,0 +1,23 @@
+const binarySearch = (array, target) => {
+  let startIndex = 0;
+  let endIndex = array.length - 1;
+  while(startIndex <= endIndex) {
+    let middleIndex = Math.floor((startIndex + endIndex) / 2);
+    if(target === array[middleIndex) {
+      return console.log("Target was found at index " + middleIndex);
+    }
+    if(target > array[middleIndex]) {
+      console.log("Searching the right side of Array")
+      startIndex = middleIndex + 1;
+    }
+    if(target < array[middleIndex]) {
+      console.log("Searching the left side of array")
+      endIndex = middleIndex - 1;
+    }
+    else {
+      console.log("Not Found this loop iteration. Looping another iteration.")
+    }
+  }
+  
+  console.log("Target value not found in array");
+}
