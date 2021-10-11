8e65830ec57d9af1a329979c609e538bce8eb1b9
pandeyastha <92038030+pandeyastha@users.noreply.github.com>
2021-10-11 18:29:15 +0530

    Compact alternative of switch

--- CPP/STACK/Valid Parenthesis.cpp
+++ CPP/STACK/Valid Parenthesis.cpp
@@ -7,34 +7,18 @@ bool isBalanced(string st)
   char x;
   for(int i=0;i<st.length();i++)
   {
-    if(st[i]=='{'||st[i]=='['||st[i]=='{')
+    if(st[i]=='{'||st[i]=='['||st[i]=='(')                         //earlier check not for round brace
     {
       s.push(st[i]);
       continue;
     }
-    if(s.empty())
-    return false;
-    switch(st[i])
-    {
-      case ')':
-      x=s.top();
-      s.pop();
-      if(x=='}'||x==']')
-      return false;
-      break;
-      case ']':
-      x=s.top();
-      s.pop();
-      if(x=='}'||x==')')
-      return false;
-      break;
-      case '}':
-      x=s.top();
-      s.pop();
-      if(x==']'||x==')')
-      return false;
-      break;
-    }
+   else if (!s.empty())
+   {
+     char c= s.top(); 
+     if(st[i]==')' &&c=='(' || st[i]==']' &&c=='[' || st[i]=='}' && c=='{')      //reduced switch case to compact form
+       s.pop();
+     else return false;
+   }
   }
   return (s.empty());
 }
