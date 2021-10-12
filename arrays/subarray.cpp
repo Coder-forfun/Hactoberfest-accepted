df1350acc7ef5396df04136bd0c70e1fbe0dde1c
aayushsamaiyar <aayushsamaiyar@gmail.com>
2021-10-12 14:58:37 +0530

    subarray,kadane algo and mergesort.c added

---
+++ arrays/subarray.cpp
@@ -0,0 +1,23 @@
+// To get all the sub arrays from an array.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+   int n; cin>>n;
+   int a[n];
+   for(int i=0;i<n;i++){
+      cin>>a[i];
+   }
+   for(int i=0;i<n;i++){
+      for(int j=i;j<n;j++){
+         for(int k=i;k<=j;k++){
+            cout<<a[k]<<" ";
+         }
+         cout<<endl;
+      }
+   }
+   return 0;
+}
+
+
