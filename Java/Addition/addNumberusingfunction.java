5abbab71576b5291bb800f575de2535c0c90c9bb
itssankarraj <56398367+itssankarraj@users.noreply.github.com>
2021-10-16 16:41:53 +0530

    Update addNumberusingfunction.java

--- Java/Addition/addNumberusingfunction.java
+++ Java/Addition/addNumberusingfunction.java
@@ -1,20 +1,10 @@
-// Sparsh Bhardwaj
-// Country - India
-// Function to add two Integer Number using Function in Java
-// Hactoberfest
+public class AddTwoNumbers {
 
-public class Main{
-  
-  public static int add(int a, int b){
-    int c = a + b;
-    return c;
-  }
-  
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in)
-    int a = sc.nextInt();
-    int b = sc.nextInt();
-    int c = addnum(a,b);
-    System.out.println(c);
-    }
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
 }
