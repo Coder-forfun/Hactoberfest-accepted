13faf29e956ee9aa531cad358503ca5595e527d8
itssankarraj <56398367+itssankarraj@users.noreply.github.com>
2021-10-16 16:40:58 +0530

    Update insertion.java

--- Java/insertion.java
+++ Java/insertion.java
@@ -1,20 +1,31 @@
 
-
-    /*Function to sort array using insertion sort*/
-    static void sort(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i = 1; i < n; ++i) { 
-            int key = arr[i]; 
-            int j = i - 1; 
-
-            /* Move elements of arr[0..i-1], that are 
-            greater than key, to one position ahead 
-            of their current position */
-            while (j >= 0 && arr[j] > key) { 
-                arr[j + 1] = arr[j]; 
-                j = j - 1; 
-            } 
-            arr[j + 1] = key; 
-        } 
-    }
+    public class InsertionSortExample {  
+        public static void insertionSort(int array[]) {  
+            int n = array.length;  
+            for (int j = 1; j < n; j++) {  
+                int key = array[j];  
+                int i = j-1;  
+                while ( (i > -1) && ( array [i] > key ) ) {  
+                    array [i+1] = array [i];  
+                    i--;  
+                }  
+                array[i+1] = key;  
+            }  
+        }  
+           
+        public static void main(String a[]){    
+            int[] arr1 = {9,14,3,2,43,11,58,22};    
+            System.out.println("Before Insertion Sort");    
+            for(int i:arr1){    
+                System.out.print(i+" ");    
+            }    
+            System.out.println();    
+                
+            insertionSort(arr1);//sorting array using insertion sort    
+               
+            System.out.println("After Insertion Sort");    
+            for(int i:arr1){    
+                System.out.print(i+" ");    
+            }    
+        }    
+    }    
