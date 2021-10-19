d6593fa103c86479e88e45bfcae47dc925df533d
rompulous <92483471+rompulous@users.noreply.github.com>
2021-10-19 14:21:17 -0700

    Create FahrenheitToCelsius.py

---
+++ Python/FahrenheitToCelsius.py
@@ -0,0 +1,12 @@
+# take input of Fahrenheit
+temp = float(input("Temperature in Fahrenheit is: "))
+
+# calculate celcius
+# subtract the 32
+temp -= 32
+
+# mulitply 
+temp = temp * (9/5)
+
+# print the celcius
+print("Temperature in Celcius is ", temp)
