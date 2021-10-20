c29b789fe69491c004dd50fe93377b6eeb86a8db
Harsh Mandaliya <32090768+hashmand@users.noreply.github.com>
2021-10-20 23:23:33 +0530

    Draw circle using Tkinter
    Draw circle using python Tkinter and canvas

---
+++ Python/drawcircle.py
@@ -0,0 +1,59 @@
+import math
+
+try:
+    import tkinter
+except ImportError:  # python 2
+    import Tkinter as tkinter
+
+
+def parabola(page, size):
+    for x in range(size):
+        y = x * x / size
+        plot(page, x, y)
+        plot(page, -x, y)
+
+
+def circle(page, radius, g, h):
+    for x in range(g, g + radius):
+        y = h + (math.sqrt(radius ** 2 - ((x-g) ** 2)))
+        plot(page, x, y)
+        plot(page, x, 2 * h - y)
+        plot(page, 2 * g - x, y)
+        plot(page, 2 * g - x, 2 * h  - y)
+
+def draw_axes(page):
+    page.update()
+    x_origin = page.winfo_width() / 2
+    y_origin = page.winfo_height() / 2
+    page.configure(scrollregion=(-x_origin, -y_origin, x_origin, y_origin))
+    page.create_line(-x_origin, 0, x_origin, 0, fill="black")
+    page.create_line(0, y_origin, 0, -y_origin, fill="black")
+    print(locals())
+
+
+def plot(page, x, y):
+    page.create_line(x, -y, x + 1, -y + 1, fill="red")
+
+mainWindow = tkinter.Tk()
+
+mainWindow.title("Parabola")
+mainWindow.geometry("640x480")
+
+canvas = tkinter.Canvas(mainWindow, width=640, height=480)
+canvas.grid(row=0, column=0)
+
+draw_axes(canvas)
+
+parabola(canvas, 100)
+parabola(canvas, 200)
+circle(canvas, 100, 100, 100)
+circle(canvas, 100, 100, -100)
+circle(canvas, 100, -100, 100)
+circle(canvas, 100, -100, -100)
+circle(canvas, 10, 30, 30)
+circle(canvas, 10, 30, -30)
+circle(canvas, 10, -30, 30)
+circle(canvas, 10, -30, -30)
+circle(canvas, 30, 0, 0)
+
+mainWindow.mainloop()
